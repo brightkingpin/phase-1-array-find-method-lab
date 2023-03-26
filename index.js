778bbfb3720b8e6cee67dcb3c12771e4662a5ee7
brightkingpin <luvandabright781@gmail.com>
2023-03-26 23:37:24 +0300

    Done with asignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+// superbowlWin function that takes an array of objects representing the Super Bowl record and returns the year in which the Denver Broncos won the Super Bowl.
+function superbowlWin(record) {
+    const winRecord = record.find(entry => entry.result === 'W');
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
